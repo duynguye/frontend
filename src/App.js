011620ac967e3dc0b0eb35b3048cab01cdaf8af0
Duy Nguyen <andy@pixeltitanstudios.com>
2019-01-13 20:18:04 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React Now! I changed it again!
           </a>
         </header>
       </div>
